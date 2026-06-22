ee44e8f37dcc783974f01dccf3e8fc3f39af8185
Er-Mayank-Aggarwal <mayank1gdsc@gmail.com>
2026-06-22 15:18:52 +0530

    fix: add fallback rewrites to proxy all unknown routes to external server to fix absolute links

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/esrc26/:path*",
-        destination: "http://68.178.167.68:8080/esrc26/:path*",
-      },
-    ];
+    return {
+      beforeFiles: [],
+      afterFiles: [
+        {
+          source: "/esrc26/:path*",
+          destination: "http://68.178.167.68:8080/esrc26/:path*",
+        },
+      ],
+      fallback: [
+        {
+          source: "/:path*",
+          destination: "http://68.178.167.68:8080/:path*",
+        },
+      ],
+    };
   },
 };
 

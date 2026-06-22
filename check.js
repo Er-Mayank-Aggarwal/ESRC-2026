11d83fb6952e2c3daf98b33100b76846d6e243d1
Er-Mayank-Aggarwal <mayank1gdsc@gmail.com>
2026-06-22 16:24:04 +0530

    cleanup: remove temporary seed button and scripts

--- check.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const txt = fs.readFileSync('src/lib/seedData.ts', 'utf8');
-const dates = new Set();
-txt.split('\n').forEach(line => {
-  if (line.includes('"date"')) {
-    dates.add(line.split('"')[3]);
-  }
-});
-console.log([...dates].sort());
